30a78219da4238c74dd3d2e8c4984eb9bc8310b4
Angelina <angel_vozh@mail.ru>
2024-12-12 23:18:17 +0300

    1_iteration

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,134 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import {
+	defaultArticleState,
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	OptionType,
+	ArticleStateType,
+} from 'src/constants/articleProps';
+import { Text } from 'src/ui/text';
+import { Separator } from 'src/ui/separator';
 import styles from './ArticleParamsForm.module.scss';
+import { useState, SyntheticEvent } from 'react';
 
-export const ArticleParamsForm = () => {
+interface IProps {
+	onSubmit: (styles: ArticleStateType) => void;
+}
+
+export const ArticleParamsForm = (props: IProps) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const [fontFamily, setFontFamily] = useState(
+		defaultArticleState.fontFamilyOption
+	);
+	const [fontSize, setFontSize] = useState(defaultArticleState.fontSizeOption);
+	const [fontColor, setFontColor] = useState(defaultArticleState.fontColor);
+	const [backgroundColor, setBackgroundColor] = useState(
+		defaultArticleState.backgroundColor
+	);
+	const [contentwidth, setContentWidth] = useState(
+		defaultArticleState.contentWidth
+	);
+
+	const handleChangeFontFamily = (selected: OptionType) => {
+		setFontFamily(selected);
+	};
+	const handleChangeFontSize = (value: OptionType) => {
+		setFontSize(value);
+	};
+	const handleChangeFontColor = (selected: OptionType) => {
+		setFontColor(selected);
+	};
+	const handleChangeBackgroundColor = (selected: OptionType) => {
+		setBackgroundColor(selected);
+	};
+	const handleChangeContentWidth = (selected: OptionType) => {
+		setContentWidth(selected);
+	};
+
+	const handleReset = (event: SyntheticEvent) => {
+		event.preventDefault();
+
+		setFontFamily(defaultArticleState.fontFamilyOption);
+		setFontSize(defaultArticleState.fontSizeOption);
+		setFontColor(defaultArticleState.fontColor);
+		setBackgroundColor(defaultArticleState.backgroundColor);
+		setContentWidth(defaultArticleState.contentWidth);
+
+		props.onSubmit(defaultArticleState);
+	};
+
+	const handleSubmit = (event: SyntheticEvent) => {
+		event.preventDefault();
+
+		const newArticleState = {
+			fontFamilyOption: fontFamily,
+			fontColor: fontColor,
+			backgroundColor: backgroundColor,
+			contentWidth: contentwidth,
+			fontSizeOption: fontSize,
+		};
+
+		props.onSubmit(newArticleState);
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
+			<aside
+				className={`${styles.container} ${
+					isOpen ? styles.container_open : ''
+				}`}>
 				<form className={styles.form}>
+					<Text weight={800} size={31} uppercase={true}>
+						{'Задайте параметры'}
+					</Text>
+					<Select
+						selected={fontFamily}
+						onChange={handleChangeFontFamily}
+						options={fontFamilyOptions}
+						title='шрифт'></Select>
+					<RadioGroup
+						name={'radio'}
+						options={fontSizeOptions}
+						selected={fontSize}
+						onChange={handleChangeFontSize}
+						title={'размер шрифта'}></RadioGroup>
+					<Select
+						selected={fontColor}
+						onChange={handleChangeFontColor}
+						options={fontColors}
+						title='цвет шрифта'></Select>
+					<Separator></Separator>
+					<Select
+						selected={backgroundColor}
+						onChange={handleChangeBackgroundColor}
+						options={backgroundColors}
+						title='цвет фона'></Select>
+					<Select
+						selected={contentwidth}
+						onChange={handleChangeContentWidth}
+						options={contentWidthArr}
+						title='ширина контента'></Select>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
+						<Button
+							title='Применить'
+							htmlType='submit'
+							type='apply'
+							onClick={handleSubmit}
+						/>
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +16,43 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [fontFamily, setFontFamily] = useState(
+		defaultArticleState.fontFamilyOption.value
+	);
+	const [fontSize, setFontSize] = useState(
+		defaultArticleState.fontSizeOption.value
+	);
+	const [fontColor, setFontColor] = useState(
+		defaultArticleState.fontColor.value
+	);
+	const [backgroundColor, setBackgroundColor] = useState(
+		defaultArticleState.backgroundColor.value
+	);
+	const [contentwidth, setContentWidth] = useState(
+		defaultArticleState.contentWidth.value
+	);
+
+	const handleChangeStyles = (styles: ArticleStateType) => {
+		setFontFamily(styles.fontFamilyOption.value);
+		setFontSize(styles.fontSizeOption.value);
+		setFontColor(styles.fontColor.value);
+		setBackgroundColor(styles.backgroundColor.value);
+		setContentWidth(styles.contentWidth.value);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': fontFamily,
+					'--font-size': fontSize,
+					'--font-color': fontColor,
+					'--container-width': contentwidth,
+					'--bg-color': backgroundColor,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm onSubmit={handleChangeStyles} />
 			<Article />
 		</main>
 	);
